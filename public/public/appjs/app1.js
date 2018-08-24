e1b371706573425580d23aa9b0a6368718468bf5
MayZinThwe <mayzinthwe62@gmail.com>
2018-08-24 14:50:51 +0630

    create project and push global

--- public/public/appjs/app1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	var database = firebase.database().ref().child("appslist");
-	database.on("child_added", snap => {
-		var package_name = snap.child("package_name").val();
-		
-		$("#appslist").append("<tr><td>"+ package_name + "</td></tr>");
-
-	});
-});
